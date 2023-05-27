d016a6affc79513417c0fafd1eb30fc83fec3307
newstein33 <65524767+seanlab1@users.noreply.github.com>
2023-05-27 12:36:26 +0900

    mod 5/27 03

--- trafficCounter/blobDetection.py
+++ trafficCounter/blobDetection.py
@@ -486,29 +486,30 @@ while ret:
 cv2.destroyAllWindows()
 print("total_cars:")
 print(total_cars)
-# import sqlite3
-# conn = sqlite3.connect("traffic2.db")
-# cur = conn.cursor()
-# conn.execute('CREATE TABLE traffic_data(id INTEGER, INTEGER, name TEXT, department TEXT, traffic_date TEXT)')
-#
-# import time
-#
-# # seconds passed since epoch
-# seconds = 1672215379.5045543
-#
-# # convert the time in seconds since the epoch to a readable format
-# local_time = time.ctime(seconds)
-#
-# print("Local time:", local_time)
-# cur.executemany(
-#     'INSERT INTO traffic_data VALUES (?, ?, ?, ?, ?)',
-#     [(1001, total_cars, local_time, 'Development', '2020-04-01 00:00:00.000'),
-#      (2001, total_cars, local_time, 'Marketing', '2020-04-01 00:00:00.000')
-#     ]
-# )
-#
-# conn.commit()
-# conn.close()
+
+
+import sqlite3
+conn = sqlite3.connect("traffic2.db")
+cur = conn.cursor()
+conn.execute('CREATE TABLE traffic_data(id INTEGER, INTEGER, name TEXT, department TEXT)')
+
+import time
+
+# seconds passed since epoch
+seconds = 1672215379.5045543
+
+# convert the time in seconds since the epoch to a readable format
+local_time = time.ctime(seconds)
+
+print("Local time:", local_time)
+cur.executemany(
+    'INSERT INTO traffic_data VALUES (?, ?, ?, ?)',
+    [(1001, total_cars, local_time, 'Development')
+    ]
+)
+
+conn.commit()
+conn.close()
 
 
 import sqlite3
